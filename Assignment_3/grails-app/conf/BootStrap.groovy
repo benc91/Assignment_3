d4186e71de929a9d183d6bdbee28817506b41302
ben <root@WebArchLinuxBase1.localdomain>
2012-05-01 19:37:36 +0100

    Assignmemt_3

--- Assignment_3/grails-app/conf/BootStrap.groovy
+++ Assignment_3/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,11 @@
+import assignment_3.Student
 class BootStrap {
 
     def init = { servletContext ->
     }
+	def benstu = new Student(name: "Ben", courseCode: "C2", applications: "2", notes: "n/a").save();
+	def tomstu = new Student(name: "Tom", courseCode: "C1", applications: "1", notes: "n/a").save();
+
     def destroy = {
     }
 }
